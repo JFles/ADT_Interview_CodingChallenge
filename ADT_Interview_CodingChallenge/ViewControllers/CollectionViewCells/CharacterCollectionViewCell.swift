3b46dc3e395b1f7ca50677cbc169073d74edf2a9
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-09-22 14:33:30 -0500

    - Added scaffolded character collection view cell - Configured collection view cell in storyboard - Migrated network code to homeCollectionViewController

---
+++ ADT_Interview_CodingChallenge/ViewControllers/CollectionViewCells/CharacterCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CharacterCollectionViewCell.swift
+//  ADT_Interview_CodingChallenge
+//
+//  Created by Jeremy Fleshman on 9/22/20.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    @IBOutlet var imageView: UIView!
+    @IBOutlet var label: UIView!
+    
+    
+}
--- ADT_Interview_CodingChallenge/ViewControllers/HomeCollectionViewController.swift
+++ ADT_Interview_CodingChallenge/ViewControllers/HomeCollectionViewController.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
-private let reuseIdentifier = "Cell"
+private let reuseIdentifier = "Character"
 
 class HomeCollectionViewController: UICollectionViewController {
+    // MARK: - Properties
+    var characters = [RMCharacter]()
+    var currentPage = 1
+    var isLoadingPage = false
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +26,46 @@ class HomeCollectionViewController: UICollectionViewController {
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
 
         // Do any additional setup after loading the view.
+        configureNavigationBar()
+        loadNetworkData(page: currentPage)
+    }
+
+    fileprivate func configureNavigationBar() {
+        title = "Rick and Morty Character List"
+    }
+
+    fileprivate func loadNetworkData(page: Int) {
+        let urlString = "https://rickandmortyapi.com/api/character/?page=\(page)"
+
+        DispatchQueue.global(qos: .userInteractive).async { [weak self] in
+            guard let strongSelf = self else { return }
+
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    strongSelf.parse(json: data)
+                } else {
+                    print("Failed to create data from network call")
+                }
+            }
+        }
+    }
+
+    fileprivate func parse(json: Data) {
+        let decoder = JSONDecoder()
+
+        if let characters = try? decoder.decode(RMCharacters.self, from: json) {
+            self.characters.append(contentsOf: characters.results)
+        } else {
+            print("Failed to decode json")
+        }
+
+        DispatchQueue.main.async { [weak self] in
+            guard let strongSelf = self else { return }
+
+            #warning("Make sure this is performant")
+            strongSelf.collectionView.reloadData()
+            strongSelf.isLoadingPage = false
+        }
     }
 
     /*
@@ -34,7 +79,6 @@ class HomeCollectionViewController: UICollectionViewController {
     */
 
     // MARK: UICollectionViewDataSource
-
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 0
@@ -43,13 +87,14 @@ class HomeCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 1
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
     
         // Configure the cell
+        
     
         return cell
     }

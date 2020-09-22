a0372a665f4ccc88b43b2ecd14ddb757a70e48cd
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-09-22 11:15:54 -0500

    - Added project folder structure - Added data models for network request for Rick and Morty character list - Set up HomeVC in storyboard - Implemented initial table view in HomeVC

---
+++ ADT_Interview_CodingChallenge/Models/RMCharacter.swift
@@ -0,0 +1,16 @@
+//
+//  RMCharacter.swift
+//  ADT_Interview_CodingChallenge
+//
+//  Created by Jeremy Fleshman on 9/22/20.
+//
+
+import Foundation
+
+struct RMCharacter: Decodable {
+    var id: Int
+    var name: String
+    var status: String
+    var species: String
+    var image: String
+}
---
+++ ADT_Interview_CodingChallenge/Models/RMCharacters.swift
@@ -0,0 +1,12 @@
+//
+//  Characters.swift
+//  ADT_Interview_CodingChallenge
+//
+//  Created by Jeremy Fleshman on 9/22/20.
+//
+
+import Foundation
+
+struct RMCharacters: Decodable {
+    var results: [RMCharacter]
+}

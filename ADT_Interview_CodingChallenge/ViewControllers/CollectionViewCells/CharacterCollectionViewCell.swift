40bcc40a2232e1f597f3e17b2d8bb37582ae379f
Jeremy Fleshman <jeremy.fleshman@icloud.com>
2020-09-22 15:14:57 -0500

    - Added custom image property configuration to custom cell class

--- ADT_Interview_CodingChallenge/ViewControllers/CollectionViewCells/CharacterCollectionViewCell.swift
+++ ADT_Interview_CodingChallenge/ViewControllers/CollectionViewCells/CharacterCollectionViewCell.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class CharacterCollectionViewCell: UICollectionViewCell {
     // MARK: - Properties
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var imageView: UIImageView! {
+        didSet {
+            configureImage()
+        }
+    }
     @IBOutlet var label: UILabel!
+
+    fileprivate func configureImage() {
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.borderWidth = 0.1
+        imageView.layer.borderColor = UIColor.lightGray.cgColor
+        imageView.layer.cornerRadius = 12
+    }
 }
